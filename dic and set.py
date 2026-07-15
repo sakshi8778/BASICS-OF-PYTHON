a6711612f943979efa5a0b06f2599c870bd58c3f
sakshi8778 <sakshi87798@gmail.com>
2026-07-15 23:36:19 +0530

    updated

--- dic and set.py
+++ dic and set.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 info = {
     "k" : "ghv",
     78 : "bjhb",
@@ -44,3 +45,51 @@ print(set1.intersection( set2 ))#commonh
     "du" : "fhtgfvg",
 } 
   
+=======
+info = {
+    "k" : "ghv",
+    78 : "bjhb",
+    "h" : ["gfg","jg","bhg"],
+    "g" : 86,
+    }
+print(info)
+print(info[78])
+info["h"] = "ghg"
+print(info)
+# null dic
+nulldic = {}
+nulldic["yt"] = " vvvhbhvb"
+print( nulldic)
+#nested dic
+student = {
+    "sub" : "chem",
+    "parts" : {
+        1 : 445,
+        2 : 788,
+    }
+}
+print(student.keys())# sub,parts
+print(list(student.keys()))#.values,.items
+print(student["parts"][2])
+student.update({"city" : "delhi"})
+
+#sets   list ,dic can't save as they are mutable
+sak = {1,4,8,"dac",6,0}
+print(type(sak))
+print(sak)
+print(len(sak))
+nullset = set()
+#set mutable
+#set element immutable
+nullset.add(4)#.remove(el),.clear(),.pop() random value
+print(nullset)
+set1 = {1,2,3}
+set2 = {3,4,5}
+print(set1.union( set2 ))#all
+print(set1.intersection( set2 ))#commonh
+"child" = {
+    "cu" : "vcghftg",
+    "du" : "fhtgfvg",
+} 
+  
+>>>>>>> 3830f1d04df5624375f4a7d5bc6e4b714c002899
